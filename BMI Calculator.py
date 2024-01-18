1e9125a90e5b7d4b283431cb4192c542d1d9d559
Wilbert Belgica <156556139+Wilbert-Belgica@users.noreply.github.com>
2024-01-18 12:07:45 +0800

    Update BMI Calculator.py

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -7,4 +7,13 @@ bmi = weight_as_int / height_as_float ** 2
 bmi = weight_as_int / (height_as_float * height_as_float)
 bmi_as_int = int(bmi)
 bmi = int(bmi)
-print("Your Body Mass Index is: " + str(bmi))
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
